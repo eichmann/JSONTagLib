ca93c9f1627cf42d79794d1cf131882b4c58dd8a
eichmann <david-eichmann@uiowa.edu>
2019-04-01 16:19:44 +0000

    The GraphQL framework is now driven by a table of queries, rather than static in-code definition strings.

--- src/main/java/org/cd2h/JSONTagLib/GraphQL/CD2H_API.java
+++ src/main/java/org/cd2h/JSONTagLib/GraphQL/CD2H_API.java
@@ -5,78 +5,10 @@ import org.cd2h.JSONTagLib.util.PropertyLoader;
 
 public class CD2H_API extends GraphQLAPI{
     static Logger logger = Logger.getLogger(CD2H_API.class);
-
-    String projects = "{"
-    	+ "  query: allProjects (orderBy:TITLE_ASC) {"
-    	+ "    nodes {"
-    	+ "      id"
-    	+ "      title"
-    	+ "      leadSite"
-    	+ "      description"
-    	+ "      pitch"
-    	+ "      github"
-    	+ "      vertical"
-    	+ "      thematicArea"
-    	+ "      rolesById (condition:{role:\"Lead\"}) {"
-    	+ "        nodes {"
-    	+ "          personByEmailAddress {"
-    	+ "            lastName"
-    	+ "            preferredFirstName"
-    	+ "          }"
-    	+ "        }"
-    	+ "      }"
-    	+ "    }"
-    	+ "  }"
-    	+ "}"
-    	;
-    String project = "{"
-    	+ "   projectById(id:$proj) {"
-    	+ "      title"
-    	+ "      leadSite"
-    	+ "      description"
-    	+ "      pitch"
-    	+ "      github"
-    	+ "      vertical"
-    	+ "      thematicArea"
-    	+ "      rolesById (condition:{role:\"Lead\"}) {"
-    	+ "        nodes {"
-    	+ "          personByEmailAddress {"
-    	+ "	       emailAddress"
-    	+ "            lastName"
-    	+ "            preferredFirstName"
-    	+ "          }"
-    	+ "        }"
-    	+ "      }"
-    	+ "	}"
-    	+ "}"
-    	;
-    String project_proper = "query($proj:Int!) {"
-	    	+ "   projectById(id:$proj) {"
-	    	+ "      title"
-	    	+ "      leadSite"
-	    	+ "      description"
-	    	+ "      pitch"
-	    	+ "      github"
-	    	+ "      vertical"
-	    	+ "      thematicArea"
-	    	+ "      rolesById (condition:{role:\"Lead\"}) {"
-	    	+ "        nodes {"
-	    	+ "          personByEmailAddress {"
-	    	+ "            lastName"
-	    	+ "            preferredFirstName"
-	    	+ "          }"
-	    	+ "        }"
-	    	+ "      }"
-	    	+ "	}"
-	    	+ "}"
-	    	;
-
-    public CD2H_API() {
+    
+     public CD2H_API() {
 	super("CD2H");
 	props = PropertyLoader.loadProperties("cd2h_postgraphile");
-	
-	registerStatement("projects", projects);
-	registerStatement("project", project);
-    }
+   }
 
 }
--- src/main/java/org/cd2h/JSONTagLib/GraphQL/GraphQLAPI.java
+++ src/main/java/org/cd2h/JSONTagLib/GraphQL/GraphQLAPI.java
@@ -7,23 +7,55 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Hashtable;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.cd2h.JSONTagLib.util.LocalProperties;
+import org.cd2h.JSONTagLib.util.PropertyLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class GraphQLAPI {
     static Logger logger = Logger.getLogger(GraphQLAPI.class);
+
+    protected static Connection conn = null;
     
     LocalProperties props = null;
     String targetServer = null;
+    boolean libraryLoaded = false;
     Hashtable<String, String> statementHash = new Hashtable<String, String>();
     Hashtable<String, String> typeHash = new Hashtable<String, String>();
     
     public GraphQLAPI(String targetServer) {
 	this.targetServer = targetServer;
+	loadStatements();
+    }
+    
+    private void loadStatements() {
+	if (libraryLoaded)
+	    return;
+	logger.info("loading " + targetServer + " graphql library");
+	try {
+	    conn = getConnection();
+	    PreparedStatement stmt = conn.prepareStatement("select name,query from graphql.query where service = ?");
+	    stmt.setString(1, targetServer);
+	    ResultSet rs = stmt.executeQuery();
+	    while (rs.next()) {
+		String name = rs.getString(1);
+		String query = rs.getString(2);
+		registerStatement(name, query);
+		logger.debug("query: " + name + "\n" + query);
+	    }
+	    conn.close();
+	} catch (SQLException | ClassNotFoundException e) {
+	    logger.error("error loading " + targetServer + "graphql library: " + e);;
+	}
     }
     
     public void registerStatement(String name, String statement) {
@@ -49,7 +81,7 @@ public class GraphQLAPI {
 	if (!search.matches(" *search.*"))
 	    throw new IOException("search request doesn't start with search");
 	// normalize search string to remove tabs, as GraphQL doesn't like them...
-	return submit(("{ \"query\": \"{" + quotify(search) + "}\" } ").replaceAll("[\t ]+", " "));
+	return submit(("{ \"query\": \"{" + quotify(search) + "}\" } ").replaceAll("[\n\t ]+", " "));
     }
 
     public JSONObject submitQuery(String query) throws IOException {
@@ -58,9 +90,9 @@ public class GraphQLAPI {
 	// normalize query string to remove tabs, as GraphQL doesn't like them...
 	switch (targetServer) {
 	case "CD2H":
-	    return submit(query.replaceAll("[\t ]+", " "));
+	    return submit(query.replaceAll("[\n\t ]+", " "));
 	default:
-	    return submit(("{ \"query\": \"query {" + quotify(query) + "}\" } ").replaceAll("[\t ]+", " "));
+	    return submit(("{ \"query\": \"query {" + quotify(query) + "}\" } ").replaceAll("[\n\t ]+", " "));
 	}
     }
 
@@ -68,12 +100,12 @@ public class GraphQLAPI {
 	if (!mutation.matches(" *update.*"))
 	    throw new IOException("mutation request doesn't start with update...");
 	// normalize mutation string to remove tabs, as GraphQL doesn't like them...
-	return submit(("{ \"query\": \"mutation {" + quotify(mutation) + "}\" } ").replaceAll("[\t ]+", " "));
+	return submit(("{ \"query\": \"mutation {" + quotify(mutation) + "}\" } ").replaceAll("[\n\t ]+", " "));
     }
 
     private JSONObject submit(String request) throws IOException {
 	// configure the connection
-	logger.info("url: " + props.getProperty("url"));
+	logger.debug("url: " + props.getProperty("url"));
 	URL uri = new URL(props.getProperty("url"));
 	HttpURLConnection con = (HttpURLConnection) uri.openConnection();
 	con.setRequestMethod("POST"); // type: POST, PUT, DELETE, GET
@@ -86,7 +118,7 @@ public class GraphQLAPI {
 	con.setDoInput(true);
 	
 	// submit the GraphQL construct
-	logger.info("request: " + request);
+	logger.debug("request: " + request);
 	BufferedWriter out = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
 	out.write(request);
 	out.flush();
@@ -105,4 +137,29 @@ public class GraphQLAPI {
     static String quotify(String theString) {
 	return theString.replace("\"", "\\\"");
     }
+
+    protected static void store(String name, String server, String query) throws SQLException {
+        System.out.println(name+"\n"+query);
+        PreparedStatement stmt = conn.prepareStatement("insert into graphql.query(name,service,query) values(?,?,?)");
+        stmt.setString(1, name);
+        stmt.setString(2, server);
+        stmt.setString(3, query);
+        stmt.execute();
+        stmt.close();
+    }
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+	LocalProperties props = PropertyLoader.loadProperties("graphql");
+	Class.forName("org.postgresql.Driver");
+    	Properties pprops = new Properties();
+    	pprops.setProperty("user", props.getProperty("jdbc.user"));
+    	pprops.setProperty("password", props.getProperty("jdbc.password"));
+    //	if (use_ssl.equals("true")) {
+    //	    props.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
+    //	    props.setProperty("ssl", "true");
+    //	}
+    	Connection conn = DriverManager.getConnection(props.getProperty("jdbc.url"), pprops);
+    	conn.setAutoCommit(true);
+    	return conn;
+        }
 }
